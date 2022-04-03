f6108b60b7d18e0fd3364bb36db5cf42fc86a869
Rajendra Prasad Gullapalli <grajpracoder@gmail.com>
2022-04-03 13:32:35 +0530

    Assignment 37 updated.

--- assignments/assign_37_create_dict_input.py
+++ assignments/assign_37_create_dict_input.py
@@ -2,3 +2,11 @@
 # Create a dictionary with fruit names as key & price as values.
 # Hint: Use any loop, input function
 
+fruits_count = 5
+fruits_dict = {}
+for i in range(fruits_count):
+    fruit_name = input("Enter fruit name: ")
+    fruit_cost_kg = input("Enter price per kg: ")
+    fruits_dict[fruit_name] = int(fruit_cost_kg)
+
+print("Fruits with price: ", fruits_dict)
